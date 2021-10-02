8f92f519a32db3e8673f9c2848d1ef2e19c1f7a1
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-02 11:44:49 +0200

    Correct algorithm

--- 2020-2021 sem I (zima)/#7 Wyjatki, regexy i roznosci/running median algorithm.py
+++ 2020-2021 sem I (zima)/#7 Wyjatki, regexy i roznosci/running median algorithm.py
@@ -1,12 +1,10 @@
 # Created by Marcin "Cozoob" Kozub 30.09.2021
 # Doesn't work
-from queue import PriorityQueue
-import heapq as hq
+from heapq import heappop, heappush
 
 def running_median(array):
     min_heap = []
-    hq.heapify(min_heap)
-    max_heap = PriorityQueue()
+    max_heap = []
     medians = []
 
     for number in array:
@@ -16,41 +14,26 @@ def running_median(array):
 
     return medians
 
-
 def add_number(number, min_heap, max_heap):
-    if len(min_heap) == 0:
-        min_heap.append(number)
+    if not max_heap or number > max_heap[0]:
+        heappush(max_heap, number)
     else:
-        lower = hq.heappop(min_heap)
-        if lower < number:
-            max_heap.put(number)
-        else:
-            hq.heappush(min_heap, number)
-
-        hq.heappush(min_heap, lower)
+        heappush(min_heap, -number) # we add a negative number to "create" a min_heap
 
 def rebalance(min_heap, max_heap):
-
-    if len(min_heap) - max_heap.qsize() > 2:
-        lower = hq.heappop(min_heap)
-        max_heap.put(lower)
+    if len(min_heap) - len(max_heap) > 1:
+        heappush(max_heap, -heappop(min_heap))  # the min_heap has too many elements
+    elif len(max_heap) - len(min_heap) > 1:
+        heappush(min_heap, -heappop(max_heap))  # the max_heap has too many elements
 
 
 def get_median(min_heap, max_heap):
-    lower = hq.heappop(min_heap)
-    if not max_heap.empty():
-        bigger = max_heap.get()
+    if len(max_heap) == len(min_heap):
+        return (max_heap[0] - min_heap[0]) / 2
+    elif len(max_heap) > len(min_heap):
+        return max_heap[0]
     else:
-        bigger = 0
-    hq.heappush(min_heap, lower)
-    max_heap.put(bigger)
-
-    if not max_heap.empty() and len(min_heap) == max_heap.qsize():
-        return (lower + bigger) / 2
-    elif len(min_heap) > max_heap.qsize():
-        return lower
-    else:
-        return bigger
+        return -min_heap[0]
 
 if __name__ == '__main__':
     arr = [3,2,6,72,45,8,4]
